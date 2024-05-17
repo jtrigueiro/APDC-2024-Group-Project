67df6ac34b2d46f09e2587c9f61e0163ec1283aa
José Trigueiro <joseleonardo2000@gmail.com>
2024-05-17 16:11:20 +0100

    firebase update

---
+++ frontend/lib/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:adc_group_project/screens/authenticate/authenticate.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
