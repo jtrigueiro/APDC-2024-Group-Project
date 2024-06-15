390ca3c0a848d725572c3217ed58f780ae1a1112
José Trigueiro <joseleonardo2000@gmail.com>
2024-06-15 19:26:41 +0100

    loading screen para signin e signup

--- apdc_2024_group_project/lib/screens/authenticate/sign_up_screen.dart
+++ apdc_2024_group_project/lib/screens/authenticate/sign_up_screen.dart
@@ -1,5 +1,6 @@
 import 'package:adc_group_project/services/auth.dart';
 import 'package:adc_group_project/shared/constants.dart';
+import 'package:adc_group_project/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:ui';
@@ -22,7 +23,7 @@ class SignUpScreenState extends State<SignUpScreen> {
   late TextEditingController passwordController;
   late TextEditingController confirmPasswordController;
   late String error = '';
-
+  bool loading = false;
   @override
   void initState() {
     scrollController = ScrollController();
@@ -36,174 +37,183 @@ class SignUpScreenState extends State<SignUpScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Sign Up'),
-        surfaceTintColor: Colors.green,
-        shadowColor: Colors.green[400],
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new_rounded),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: Scrollbar(
-        controller: scrollController,
-        child: SingleChildScrollView(
-          controller: scrollController,
-          child: Container(
-            decoration: const BoxDecoration(
-              color: Color(0xFFFFFFFF),
+    return loading
+        ? const Loading()
+        : Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              title: const Text('Sign Up'),
+              surfaceTintColor: Colors.green,
+              shadowColor: Colors.green[400],
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                //para a imagem
-                Container(
-                  margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
-                  child: SizedBox(
-                    width: 70,
-                    height: 70,
-                    child: SvgPicture.asset(
-                      "assets/vectors/logo.svg",
-                    ),
+            body: Scrollbar(
+              controller: scrollController,
+              child: SingleChildScrollView(
+                controller: scrollController,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFFFFFFF),
                   ),
-                ),
-                //text
-                Container(
-                  margin: const EdgeInsets.fromLTRB(0, 0, 3.7, 0),
-                  child: Text(
-                    'Sign Up',
-                    style: GoogleFonts.getFont(
-                      'Nunito',
-                      fontWeight: FontWeight.w700,
-                      fontSize: 40,
-                      color: const Color(0xFF000000),
-                    ),
-                  ),
-                ),
-
-                //forms
-                Container(
-                  margin: const EdgeInsets.fromLTRB(18.2, 20, 16.1, 50),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
+                      //para a imagem
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+                        child: SizedBox(
+                          width: 70,
+                          height: 70,
+                          child: SvgPicture.asset(
+                            "assets/vectors/logo.svg",
+                          ),
+                        ),
+                      ),
+                      //text
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 3.7, 0),
+                        child: Text(
+                          'Sign Up',
+                          style: GoogleFonts.getFont(
+                            'Nunito',
+                            fontWeight: FontWeight.w700,
+                            fontSize: 40,
+                            color: const Color(0xFF000000),
+                          ),
+                        ),
+                      ),
+
                       //forms
-                      Form(
-                        key: _formKey,
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(18.2, 20, 16.1, 50),
                         child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              TextFormField(
-                                validator: (val) =>
-                                    val!.isEmpty ? 'Enter a name' : null,
-                                controller: nameController,
-                                decoration: textInputDecoration.copyWith(
-                                    labelText: 'Name'),
-                              ),
+                          children: [
+                            //forms
+                            Form(
+                              key: _formKey,
+                              child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    TextFormField(
+                                      validator: (val) =>
+                                          val!.isEmpty ? 'Enter a name' : null,
+                                      controller: nameController,
+                                      decoration: textInputDecoration.copyWith(
+                                          labelText: 'Name'),
+                                    ),
 
-                              //email
-                              TextFormField(
-                                validator: (val) =>
-                                    val!.isEmpty ? 'Enter an email' : null,
-                                controller: emailController,
-                                decoration: textInputDecoration.copyWith(
-                                    labelText: 'Email'),
-                              ),
+                                    //email
+                                    TextFormField(
+                                      validator: (val) => val!.isEmpty
+                                          ? 'Enter an email'
+                                          : null,
+                                      controller: emailController,
+                                      decoration: textInputDecoration.copyWith(
+                                          labelText: 'Email'),
+                                    ),
 
-                              //password
-                              TextFormField(
-                                validator: (val) => val!.isEmpty
-                                    ? 'Enter a password'
-                                    : (val.length < 6
-                                        ? 'Enter a password with 6 or more characters'
-                                        : null),
-                                obscureText: true,
-                                controller: passwordController,
-                                decoration: textInputDecoration.copyWith(
-                                    labelText: 'Password'),
-                              ),
+                                    //password
+                                    TextFormField(
+                                      validator: (val) => val!.isEmpty
+                                          ? 'Enter a password'
+                                          : (val.length < 6
+                                              ? 'Enter a password with 6 or more characters'
+                                              : null),
+                                      obscureText: true,
+                                      controller: passwordController,
+                                      decoration: textInputDecoration.copyWith(
+                                          labelText: 'Password'),
+                                    ),
 
-                              //confirm password
-                              TextFormField(
-                                validator: (val) => val!.isEmpty
-                                    ? 'Confirm your password'
-                                    : (val != passwordController.text
-                                        ? 'Wrong confirmation password'
-                                        : null),
-                                obscureText: true,
-                                controller: confirmPasswordController,
-                                decoration: textInputDecoration.copyWith(
-                                    labelText: 'Confirm Password'),
-                              ),
-                            ]),
-                      ),
+                                    //confirm password
+                                    TextFormField(
+                                      validator: (val) => val!.isEmpty
+                                          ? 'Confirm your password'
+                                          : (val != passwordController.text
+                                              ? 'Wrong confirmation password'
+                                              : null),
+                                      obscureText: true,
+                                      controller: confirmPasswordController,
+                                      decoration: textInputDecoration.copyWith(
+                                          labelText: 'Confirm Password'),
+                                    ),
+                                  ]),
+                            ),
 
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        style: ButtonStyle(
-                          foregroundColor: MaterialStateProperty.all<Color>(
-                              const Color.fromARGB(255, 130, 196, 112)),
-                          overlayColor: MaterialStateProperty.all(
-                              const Color.fromARGB(255, 221, 223, 194)),
-                        ),
-                        child: const Text('Already have an account? Login!'),
-                      ),
-
-                      //login button
-                      ElevatedButton(
-                        onPressed: () async {
-                          {
-                            if (_formKey.currentState!.validate()) {
-                              dynamic result =
-                                  await _auth.registerWithEmailAndPassword(
-                                      emailController.text,
-                                      passwordController.text);
-                              if (result == null) {
-                                setState(
-                                  () => error =
-                                      'Email not valid or already in use.',
-                                );
-                              } else {
+                            TextButton(
+                              onPressed: () {
                                 Navigator.pop(context);
-                              }
-                            }
-                          }
-                        },
-                        style: ButtonStyle(
-                          foregroundColor: MaterialStateProperty.all<Color>(
-                              const Color(0xFFFFFFFF)),
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              const Color.fromARGB(255, 130, 196, 112)),
+                              },
+                              style: ButtonStyle(
+                                foregroundColor: MaterialStateProperty.all<
+                                        Color>(
+                                    const Color.fromARGB(255, 130, 196, 112)),
+                                overlayColor: MaterialStateProperty.all(
+                                    const Color.fromARGB(255, 221, 223, 194)),
+                              ),
+                              child:
+                                  const Text('Already have an account? Login!'),
+                            ),
+
+                            //login button
+                            ElevatedButton(
+                              onPressed: () async {
+                                {
+                                  if (_formKey.currentState!.validate()) {
+                                    setState(() => loading = true);
+                                    dynamic result = await _auth
+                                        .registerWithEmailAndPassword(
+                                            emailController.text,
+                                            passwordController.text);
+                                    if (result == null) {
+                                      setState(() {
+                                        loading = false;
+                                        error =
+                                            'Email not valid or already in use.';
+                                      });
+                                    } else {
+                                      Navigator.pop(context);
+                                    }
+                                  }
+                                }
+                              },
+                              style: ButtonStyle(
+                                foregroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        const Color(0xFFFFFFFF)),
+                                backgroundColor: MaterialStateProperty.all<
+                                        Color>(
+                                    const Color.fromARGB(255, 130, 196, 112)),
+                              ),
+                              child: const Text('Sign Up'),
+                            ),
+                          ],
                         ),
-                        child: const Text('Sign Up'),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                        child: Text(
+                          error,
+                          style: GoogleFonts.getFont(
+                            'Nunito',
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 255, 0, 0),
+                          ),
+                        ),
                       ),
                     ],
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                  child: Text(
-                    error,
-                    style: GoogleFonts.getFont(
-                      'Nunito',
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16,
-                      color: Color.fromARGB(255, 255, 0, 0),
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
-    );
+          );
   }
 }
