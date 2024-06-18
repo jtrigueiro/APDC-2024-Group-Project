1c1b7949c21f7df21f5afa016b05f31a7a2f4681
José Trigueiro <joseleonardo2000@gmail.com>
2024-06-18 15:00:00 +0100

    register restaurant loading screen when waiting for network communication

--- apdc_2024_group_project/lib/screens/profile/profile_subscreen/my_restaurant/no_restaurant_screen.dart
+++ apdc_2024_group_project/lib/screens/profile/profile_subscreen/my_restaurant/no_restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:adc_group_project/screens/profile/profile_subscreen/my_restaurant/restaurant_requested_screen.dart';
 import 'package:adc_group_project/services/auth.dart';
+import 'package:adc_group_project/shared/loading.dart';
 //import 'package:adc_group_project/services/auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -9,7 +10,7 @@ import 'package:google_fonts/google_fonts.dart';
 class NoRestaurantScreen extends StatefulWidget {
   //const NoRestaurantScreen({super.key});
 
-  Function checkCurrentIndex;
+  final Function checkCurrentIndex;
   NoRestaurantScreen({
     Key? key,
     required this.checkCurrentIndex,
@@ -24,7 +25,7 @@ class NoRestaurantScreenState extends State<NoRestaurantScreen> {
   late TextEditingController nameController;
   late TextEditingController phoneController;
   late TextEditingController locationController;
-
+  bool loading = false;
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
 
@@ -40,72 +41,84 @@ class NoRestaurantScreenState extends State<NoRestaurantScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Scrollbar(
-        controller: scrollController,
-        child: SingleChildScrollView(
-          controller: scrollController,
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Wrap(
-                  direction: Axis.vertical,
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  spacing: 10,
-                  children: [
-                    texts('Seems like you have no restaurant yet!', 20),
-                    texts('Add one now!', 20),
-                  ],
-                ),
-                Form(
-                  key: _formKey,
-                  child: Column(children: [
-                    const SizedBox(height: 50),
-                    textForms(nameController, 'Restaurant Name',
-                        'Please enter a restaurant name'),
-                    const SizedBox(height: 10),
-                    textForms(phoneController, 'Phone number',
-                        'Please enter a phone number'),
-                    const SizedBox(height: 10),
-                    textForms(locationController, 'Location',
-                        'Please enter a location'),
-                    const SizedBox(height: 30),
-                  ]),
-                ),
-                Container(
-                  alignment: Alignment.bottomLeft,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        dynamic result =
-                            await _auth.registerRestaurantApplication(
-                                nameController.text,
-                                phoneController.text,
-                                locationController.text);
-                        if (result == null) {
-                          // TODO: expor um erro de como falhou o envio
-                        } else {
-                          widget.checkCurrentIndex();
-                        }
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      textStyle: const TextStyle(fontStyle: FontStyle.italic),
-                      backgroundColor: Colors.green[100],
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                    ),
-                    child: const Text('Send'),
+    return loading
+        ? const Loading()
+        : Scaffold(
+            body: Scrollbar(
+              controller: scrollController,
+              child: SingleChildScrollView(
+                controller: scrollController,
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Wrap(
+                        direction: Axis.vertical,
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        spacing: 10,
+                        children: [
+                          texts('Seems like you have no restaurant yet!', 20),
+                          texts('Add one now!', 20),
+                        ],
+                      ),
+                      Form(
+                        key: _formKey,
+                        child: Column(children: [
+                          const SizedBox(height: 50),
+                          textForms(nameController, 'Restaurant Name',
+                              'Please enter a restaurant name'),
+                          const SizedBox(height: 10),
+                          textForms(phoneController, 'Phone number',
+                              'Please enter a phone number'),
+                          const SizedBox(height: 10),
+                          textForms(locationController, 'Location',
+                              'Please enter a location'),
+                          const SizedBox(height: 30),
+                        ]),
+                      ),
+                      Container(
+                        alignment: Alignment.bottomLeft,
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              setState(() {
+                                loading = true;
+                              });
+                              dynamic result =
+                                  await _auth.registerRestaurantApplication(
+                                      nameController.text,
+                                      phoneController.text,
+                                      locationController.text);
+                              if (result == null) {
+                                setState(() {
+                                  loading = false;
+                                });
+                                // TODO: expor um erro de como falhou o envio, fazer como eu fiz no signup ou signin - jose
+                              } else {
+                                widget.checkCurrentIndex();
+                                setState(() {
+                                  loading = false;
+                                });
+                              }
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            textStyle:
+                                const TextStyle(fontStyle: FontStyle.italic),
+                            backgroundColor: Colors.green[100],
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                          ),
+                          child: const Text('Send'),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
-    );
+          );
   }
 
   TextFormField textForms(
