e5f8d3fe9d9bd25a2ce87ca253a76dd991228bcf
DiogoLemos <145502790+dblemos@users.noreply.github.com>
2024-06-19 22:05:55 +0100

    Things

--- apdc_2024_group_project/lib/screens/profile/profile_subscreen/my_restaurant/no_restaurant_screen.dart
+++ apdc_2024_group_project/lib/screens/profile/profile_subscreen/my_restaurant/no_restaurant_screen.dart
@@ -1,11 +1,13 @@
-import 'package:adc_group_project/services/auth.dart';
+import 'dart:async';
+import 'dart:convert';
 import 'package:adc_group_project/services/database.dart';
 import 'package:adc_group_project/utils/constants.dart';
 import 'package:adc_group_project/utils/loading_screen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
 
 class NoRestaurantScreen extends StatefulWidget {
   //const NoRestaurantScreen({super.key});
@@ -21,12 +23,20 @@ class NoRestaurantScreen extends StatefulWidget {
 }
 
 class NoRestaurantScreenState extends State<NoRestaurantScreen> {
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+  final LatLng _center = const LatLng(38.660259532890706, -9.203190255573041);
+  final String apiKey = 'AIzaSyBYDIEadA1BKbZRNEHL1WFI8PWFdXKI5ug';
+
+  late Marker marker;
+  
   late ScrollController scrollController;
   late TextEditingController nameController;
   late TextEditingController phoneController;
-  late TextEditingController locationController;
+  late TextEditingController addressController;
+  late GoogleMapController mapController;
+
+  String? _mapStyle;
   bool loading = false;
-  final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -34,10 +44,47 @@ class NoRestaurantScreenState extends State<NoRestaurantScreen> {
     scrollController = ScrollController();
     nameController = TextEditingController();
     phoneController = TextEditingController();
-    locationController = TextEditingController();
+    addressController = TextEditingController();
 
+    _loadMapStyle();
     super.initState();
   }
+
+  Future<void> _loadMapStyle() async {
+    _mapStyle = await rootBundle.loadString('assets/map_style.json');
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+    if (_mapStyle != null) {
+      controller.setMapStyle(_mapStyle);
+    }
+  }
+
+  void _handleTap(LatLng position) async {
+    final url = Uri.parse('https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$apiKey');
+    final response = await http.get(url);
+    dynamic result;
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      if (json['status'] == 'OK') {
+        result = json['results'][0]['formatted_address'];
+      } else {
+        print('Error: ${json['status']}');
+        return null;
+      }
+    } else {
+      print('Failed to fetch address');
+      return null;
+    }
+
+    addressController.text = result;
+
+    setState(() {
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -69,8 +116,21 @@ class NoRestaurantScreenState extends State<NoRestaurantScreen> {
                           const SizedBox(height: 10),
                           textForms(phoneController, 'Phone number*', 'Please enter a phone number'),
                           const SizedBox(height: 10),
-                          textForms(locationController, 'Location*', 'Please enter a location'),
+                          textForms(addressController, 'Adress*', 'Please enter an address'),
                           const SizedBox(height: 30),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            height: 300,
+                            child: GoogleMap(
+                              onMapCreated: _onMapCreated,
+                              initialCameraPosition: CameraPosition(
+                                target: _center,
+                                zoom: 14.0,
+                              ),
+                              onTap: _handleTap,
+                            ),
+                          ),
+
                         ]
                         ),
                       ),
@@ -79,29 +139,29 @@ class NoRestaurantScreenState extends State<NoRestaurantScreen> {
                         alignment: Alignment.bottomLeft,
                         child: ElevatedButton(
                           onPressed: () async {
-                             if (_formKey.currentState!.validate()) {
-                               setState(() {
-                                 loading = true;
-                               });
-                               dynamic result = await DatabaseService()
-                                   .addOrUpdateRestaurantApplicationData(
+                            if (_formKey.currentState!.validate()) {
+                              setState(() {
+                                loading = true;
+                              });
+                              dynamic result = await DatabaseService()
+                                  .addOrUpdateRestaurantApplicationData(
                                       nameController.text,
                                       phoneController.text,
-                                      locationController.text);
+                                      addressController.text);
 
-                               if (result == null) {
-                                  setState(() {
-                                    loading = false;
-                                  });
-                                  // TODO: expor um erro de como falhou o envio, fazer como eu fiz no signup ou signin - jose
-                               } else {
-                                  widget.checkCurrentIndex();
-                                  setState(() {
-                                    loading = false;
-                                  });
-                               }
-                             }
-                             },
+                              if (result == null) {
+                                setState(() {
+                                  loading = false;
+                                });
+                                // TODO: expor um erro de como falhou o envio, fazer como eu fiz no signup ou signin - jose
+                              } else {
+                                widget.checkCurrentIndex();
+                                setState(() {
+                                  loading = false;
+                                });
+                              }
+                            }
+                          },
                           child: const Text('Send'),
                         ),
                       ),
@@ -111,6 +171,40 @@ class NoRestaurantScreenState extends State<NoRestaurantScreen> {
               ),
             ),
           ),
+        );
+  }
+
+  TextFormField textForms(
+      TextEditingController controller, text, String textNoValue) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+          labelText: '$text*',
+          labelStyle:
+              const TextStyle(fontStyle: FontStyle.italic, color: Colors.black),
+          border: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.green),
+              borderRadius: BorderRadius.circular(10)),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green),
+          )),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return textNoValue;
+        }
+        return null;
+      },
+    );
+  }
+
+  Text texts(String text, double size) {
+    return Text(
+      text,
+      style: GoogleFonts.getFont(
+        'Nunito',
+        fontWeight: FontWeight.normal,
+        fontSize: size,
+      ),
     );
   }
 }
