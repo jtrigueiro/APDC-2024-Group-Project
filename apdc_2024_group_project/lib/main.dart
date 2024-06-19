5ae0e70e8d729ea24749dc3d7739938aa14f29f9
LillyMC20 <lmo.correia@campus.fct.unl.pt>
2024-06-19 23:46:46 +0100

    portrait onlny mode

--- apdc_2024_group_project/lib/main.dart
+++ apdc_2024_group_project/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:adc_group_project/utils/themes/theme.dart';
 import 'package:adc_group_project/wrapper.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:adc_group_project/services/models/user.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -11,7 +12,10 @@ import 'package:firebase_core/firebase_core.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
   runApp(MyApp());
 }
 
