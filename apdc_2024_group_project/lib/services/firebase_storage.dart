d8b08bfd730c199853c3d44a1e80c073ee6c9e45
José Trigueiro <joseleonardo2000@gmail.com>
2024-07-03 00:06:35 +0100

    firebase storage call correction

--- apdc_2024_group_project/lib/services/firebase_storage.dart
+++ apdc_2024_group_project/lib/services/firebase_storage.dart
@@ -4,6 +4,8 @@ import 'package:firebase_storage/firebase_storage.dart';
 
 // firebase storage service
 class StorageService {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
   String dishImagePath(String uid, String dishId) {
     return 'restaurants/$uid/dishes/$dishId';
   }
@@ -15,14 +17,14 @@ class StorageService {
   Future uploadDishImageMobile(
       String uid, String dishId, String imagePath) async {
     final path = dishImagePath(uid, dishId);
-    final ref = FirebaseStorage.instance.ref().child(path);
+    final ref = _storage.ref().child(path);
     await ref.putFile(File(imagePath));
   }
 
   Future uploadDishImageWeb(String uid, String dishId, Uint8List imageBytes,
       String imageExtension) async {
     final path = dishImagePath(uid, dishId);
-    final ref = FirebaseStorage.instance.ref().child(path);
+    final ref = _storage.ref().child(path);
     final extensionName = imageExtension.split('.').last;
     final metadata = SettableMetadata(contentType: 'image/$extensionName');
     await ref.putData(imageBytes, metadata);
@@ -30,26 +32,26 @@ class StorageService {
 
   Future deleteDishImage(String uid, String dishId) async {
     final path = dishImagePath(uid, dishId);
-    final ref = FirebaseStorage.instance.ref().child(path);
+    final ref = _storage.ref().child(path);
     await ref.delete();
   }
 
   Future<String> getDishImageUrl(String uid, String dishId) async {
     final path = dishImagePath(uid, dishId);
-    final ref = FirebaseStorage.instance.ref().child(path);
+    final ref = _storage.ref().child(path);
     return await ref.getDownloadURL();
   }
 
   Future uploadRestaurantImageMobile(String uid, String imagePath) async {
     final path = restaurantImagePath(uid);
-    final ref = FirebaseStorage.instance.ref().child(path);
+    final ref = _storage.ref().child(path);
     await ref.putFile(File(imagePath));
   }
 
   Future uploadRestaurantImageWeb(
       String uid, Uint8List imageBytes, String imageExtension) async {
     final path = restaurantImagePath(uid);
-    final ref = FirebaseStorage.instance.ref().child(path);
+    final ref = _storage.ref().child(path);
     final extensionName = imageExtension.split('.').last;
     final metadata = SettableMetadata(contentType: 'image/$extensionName');
     await ref.putData(imageBytes, metadata);
@@ -57,7 +59,7 @@ class StorageService {
 
   Future getRestaurantImageUrl(String uid) async {
     final path = restaurantImagePath(uid);
-    final ref = FirebaseStorage.instance.ref().child(path);
+    final ref = _storage.ref().child(path);
     return await ref.getDownloadURL();
   }
 }
