a64def63bdfabfc93fe1bf1e7dcc6944550f0f96
DiogoLemos <145502790+dblemos@users.noreply.github.com>
2024-07-04 18:08:07 +0100

    changes made

---
+++ apdc_2024_group_project/lib/services/geocoding.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class GeocodingService {
+  static const String apiKey = 'AIzaSyBYDIEadA1BKbZRNEHL1WFI8PWFdXKI5ug';
+
+  Future<Map<String, dynamic>> geocode(String address) async {
+    final encodedAddress = Uri.encodeComponent(address);
+    final url = 'https://maps.googleapis.com/maps/api/geocode/json?address=$encodedAddress&key=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+    final decodedResponse = json.decode(response.body);
+
+    if (response.statusCode == 200) {
+      return decodedResponse;
+    } else {
+      throw Exception('Failed to geocode address');
+    }
+  }
+
+  Future<Map<String, dynamic>> reverseGeocode(double latitude, double longitude) async {
+    final url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+    final decodedResponse = json.decode(response.body);
+
+    if (response.statusCode == 200) {
+      return decodedResponse;
+    } else {
+      throw Exception('Failed to reverse geocode coordinates');
+    }
+  }
+}
